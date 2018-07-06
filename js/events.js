0781cbfb9027e8b1e48905a37ab36407b6dfc7ac
scalande@yahoo.com <scalande@yahoo.com>
2018-07-06 08:03:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
-//define functions here
+function getIt(){
+  $("p").on("click", function(){
+    window.alert("Hey!");
+    });
+  }
+  
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
 
-$(document).ready(function(){
+function pressIt(){
+  $("input").on("keydown", function(e){
+    if (e.which === 71){
+      window.alert("G was pressed.");
+    }
+  });
+}
 
-// call functions here
+function submitIt(){
+  $("form").on("submit", function(){
+    window.alert("Your form is going to be submitted now.");
+  });
+}
 
-});
+getIt();
+frameIt();
+pressIt();
+submitIt();
